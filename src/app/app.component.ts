9da3783961e3cd5a5537cd76ce0b6efc2646849f
islambagis <119063658+Bagisislam@users.noreply.github.com>
2024-04-15 18:39:48 +0300

    ProductComponenetCreated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
